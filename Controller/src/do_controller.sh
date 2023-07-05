2563b90e8bbb1699218a6a7cddb0ed76f19e2ab1
strongti <96398568+strongti@users.noreply.github.com>
2023-07-05 22:43:28 +0900

    Add files via upload

---
+++ Controller/src/do_controller.sh
@@ -0,0 +1,1 @@
+VSOMEIP_CONFIGURATION=../json/vsomeip_client.json VSOMEIP_APPLICATION_NAME=client-sample ./controller
---
+++ Controller/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <CommonAPI/CommonAPI.hpp>
+#include <v1/commonapi/HackathonProxy.hpp>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "Accel.h"
+#include "Order.h"
+#include "Rpm.h"
+#include <qqml.h>
+
+using namespace v1_0::commonapi;
+
+int main(int argc, char *argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    qmlRegisterType<Accel>("com.yourcompany.accel", 1, 0, "Accel");
+    qmlRegisterType<Order>("com.yourcompany.order", 1, 0, "Order");
+    qmlRegisterType<Rpm>("com.yourcompany.rpm", 1, 0, "Rpm");
+
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+
+
+
+
+    return app.exec();
+}
+
